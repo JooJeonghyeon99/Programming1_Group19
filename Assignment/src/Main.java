68cd15d45d2a949a501178869e462922ba0410ea
Minh Thong <73870657+minhthong0480@users.noreply.github.com>
2021-09-05 23:07:34 +0700

    Update Main.java

--- Assignment/src/Main.java
+++ Assignment/src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-//        Data d = new Data();
-//        d.zoneInput();
-//        d.timeRange();
-//        Summary s = new Summary();
-//        s.displaySummary();
+        Data d = new Data();
+        d.zoneInput();
+        d.timeRange();
+        Summary s = new Summary();
+        s.displaySummary();
+        MenuUI m = new MenuUI();
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                m.createAndShowGUI();
+            }
+        });
+
     }
 }
-
-
-
